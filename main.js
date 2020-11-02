2e1323d2a5d91aded7d2ec783fc8b525d3d5e70b
Caio Haag <caiohaag@gmail.com>
2020-11-02 19:49:26 -0300

    link selected

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ window.addEventListener("wheel", function (event) {
     var sectionOld = $("input[name=grupo]:checked").prop("id");
     if (sectionOld == "link1") return;
     $("." + sectionOld).fadeOut();
+    $(".label").removeClass('selected');    
     sectionScroll = true;
     $("input[name=grupo]:checked")
       .prevAll(":radio:first")
       .prop("checked", true);
     var sectionNew = $("input[name=grupo]:checked").prop("id");
+    linkSelected();
     setTimeout(function () {
       $("." + sectionNew).fadeIn();
     }, 400);
@@ -21,12 +23,14 @@ window.addEventListener("wheel", function (event) {
   } else if (event.deltaY > 0) {
     var sectionOld = $("input[name=grupo]:checked").prop("id");
     if (sectionOld == "link5") return;
-    $("." + sectionOld).fadeOut();
+    $("." + sectionOld).fadeOut();    
+    $(".label").removeClass('selected'); 
     sectionScroll = true;
     $("input[name=grupo]:checked")
       .nextAll(":radio:first")
       .prop("checked", true);
     var sectionNew = $("input[name=grupo]:checked").prop("id");
+    linkSelected();
     setTimeout(function () {
       $("." + sectionNew).fadeIn();
     }, 400);
@@ -39,8 +43,16 @@ window.addEventListener("wheel", function (event) {
 
 $('input[type=radio][name=grupo]').change(function() {
   $(".caravanita").fadeOut();
+  $(".label").removeClass('selected');
+  linkSelected();
   var sectionNewClick = $("input[name=grupo]:checked").prop("id");
   setTimeout(function () {
   $("." + sectionNewClick).fadeIn();
 }, 400);
 });
+
+
+function linkSelected() {  
+  var label = $("label[for='" + $($("input[name=grupo]:checked")).attr('id') + "']");
+  label.addClass('selected');
+}
