af8f8e434dfd8efca18327f1d8ed14bfbb8d5a07
Caio Haag <36110631+caiohaag@users.noreply.github.com>
2023-08-04 10:08:35 -0300

    Update testEmbed.js

--- testEmbed.js
+++ testEmbed.js
@@ -3,3 +3,7 @@ console.log('working well');
 const header = document.getElementById('unsetGridHeader');
 
 header.remove();
+
+setTimeout(() => {
+    window.location.href = "http://www.google.com.br";
+  }, 2000)
