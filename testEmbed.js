ee74ed0e925b3b536cbdbe22bf71c2321fda820d
caiohaagvilt <caio.haag@vilt-group.com>
2023-08-07 10:44:00 -0300

    change locator script

--- testEmbed.js
+++ testEmbed.js
@@ -1,15 +1,11 @@
 setTimeout(() => {
-    console.log('Redirecting in 3...');
+    console.log('Script loaded');
   }, 1000)
 
-setTimeout(() => {
-    console.log('Redirecting in 2...');
-  }, 2000)
-
-setTimeout(() => {
-    console.log('Redirecting in 1...');
-  }, 3000)
-
-setTimeout(() => {
-    window.location.href = "http://www.google.com.br";
-  }, 4000)
+!function(){
+  const t=document.currentScript;
+  if(!t||!t.src)return void console.log("Could not find script source URL");
+  const c=function(o){return"https://packages.prmcdn.io/locator/0.4.3/"+o},r=c("locator-app.js");
+  r!==t.src?window.addEventListener("load",()=>{
+    ["vendor.js","polyfills.js"].forEach(o=>{document.getElementsByTagName("body")[0].lastElementChild.insertAdjacentHTML("afterend",`<script src="${c(o)}"><\/script>`)}),import(r).then(o=>{o.get("./App").then(l=>{const a=l(),e=t.getAttribute("data-config-element"),s=e?document.querySelector(e):t;if(!s)return void console.error('Could not find "'+e+'". Please add a selector to the page');const i=t.getAttribute("data-config");a.mount(s,i)})})
+  }):console.log("No locator script found")}();
